063c9bdfb15ffe45e0362a4c47195f7450371702
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-04-03 11:45:26 +0530

    Update leave.sh

--- rofi/leave/leave.sh
+++ rofi/leave/leave.sh
@@ -8,7 +8,7 @@ elif [[ $choice == "Logout" ]];then
 elif [[ $choice == "Suspend" ]];then
     systemctl suspend
 elif [[ $choice == "Reboot" ]];then
-    reboot
-elif [[ $choice == "Reboot" ]];then
-    poweroff
+    systemctl reboot
+elif [[ $choice == "Shutdown" ]];then
+    systemctl poweroff
 fi
